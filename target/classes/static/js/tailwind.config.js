ae05db30c5e9c89e22af1b8cf016957bad3d3d07
Suman Talukdar <112842864+jiraiyasuman@users.noreply.github.com>
2025-07-28 00:32:58 +0530

    Add files via upload

---
+++ target/classes/static/js/tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+module.exports = {
+  content: [
+    './src/main/resources/templates/**/*.html', // Thymeleaf templates
+    './src/main/resources/static/js/**/*.js',   // Optional JS scanning
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
